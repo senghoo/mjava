3550a6e27c404c872bc994812f37077b10a4dd4a
Senghoo Kim <shkdmb@gmail.com>
2019-10-02 15:17:06 +0800

    finish nfa

--- lex.lisp
+++ lex.lisp
@@ -25,27 +25,27 @@
 (defun nfa-new-nodes (symbol tail head)
   (list (cons :symbol symbol) (cons :tail tail) (cons :head  head)))
 
-(defun nfa--copy-nodes (id seen)
-  (let ((edges (nfa-edges id))
-        (new-id (nfa-new-state)))
+(defun nfa--copy-nodes (graph id seen)
+  (let ((edges (nfa-edges graph id))
+        (new-id (nfa-new-state graph)))
+    (setf seen (acons id new-id seen))
     (mapcar
      #'(lambda (edge)
-         )
+         (let ((link-to
+               (or (cdr (assoc (cdr edge) seen))
+                   (multiple-value-bind (new-id new-seen)
+                       (nfa--copy-nodes graph (cdr edge) seen)
+                     (setf seen new-seen)
+                     new-id))))
+           (nfa-new-edge graph new-id link-to (car edge))))
      edges)
-    ))
+    (values new-id seen)))
 
-(defun nfa-copy-nodes (nodes &optional) copied
-  (let* ((symbol (nfa-symbol nodes))
-         (head (nfa-head nodes))
-         (tail (nfa-tail nodes))
-         (new-head (nfa-new-state))
-         (new-copied (if copied
-                         (acons head new-head copied)
-                         (list (cons head new-head) (cons tail (nfa-new-state)))))))
-  (mapcar
-   #'(lambda ((symbol target))
-       )
-   ))
+(defun nfa-copy-nodes (graph nodes)
+  (let ((new-head (nfa-new-state graph)))
+    (multiple-value-bind (new-tail)
+        (nfa--copy-nodes graph (nfa-tail nodes) (acons (nfa-head nodes) new-head nil))
+      (nfa-new-nodes (nfa-symbol nodes) new-head new-tail))))
 
 (defun nfa-tail (nodes)
   (cdr (assoc :tail nodes)))
@@ -59,7 +59,7 @@
 (defun nfa-epsilon (graph)
   (nfa-simple-symbol graph nil))
 
-(defun nfa-or (graph a b)
+(defun nfa--or (graph a b)
   (let ((tail (nfa-new-state graph))
         (head (nfa-new-state graph)))
     (nfa-new-edge graph tail (nfa-tail a) (nfa-symbol a))
@@ -68,17 +68,43 @@
     (nfa-new-edge graph (nfa-head b) head nil)
     (nfa-new-nodes nil tail head)))
 
+(defun nfa-or (graph a b)
+  (let ((new-a (nfa-copy-nodes graph a))
+        (new-b (nfa-copy-nodes graph b)))
+    (nfa--or graph new-a new-b)))
 
-(defun nfa-dot (graph a b)
+
+(defun nfa--dot (graph a b)
   (nfa-new-edge graph (nfa-head a) (nfa-tail b) (nfa-symbol b))
   (nfa-new-nodes (nfa-symbol a) (nfa-tail a) (nfa-head b)))
 
+(defun nfa-dot (graph a b)
+  (let ((new-a (nfa-copy-nodes graph a))
+        (new-b (nfa-copy-nodes graph b)))
+    (nfa--dot graph new-a new-b)))
 
-(defun nfa-star (graph a)
-  (let ((head (nfa-new-state graph)))
-    (nfa-new-edge graph (nfa-head a) head nil)
-    (nfa-new-edge graph head (nfa-tail a) (nfa-symbol a))
+
+(defun nfa-* (graph a)
+  (let ((head (nfa-new-state graph))
+        (new-a (nfa-copy-nodes graph a)))
+    (nfa-new-edge graph (nfa-head new-a) head nil)
+    (nfa-new-edge graph head (nfa-tail new-a) (nfa-symbol new-a))
     (nfa-new-nodes nil head head)))
 
-(defun nfa-plus (graph a)
-  (m))
+(defun nfa-+(graph a)
+  (nfa-dot graph a (nfa-star graph a)))
+
+(defun nfa-?(graph a)
+  (nfa-dot graph a (nfa-star graph a)))
+
+(defun nfa-[] (graph &rest args)
+  (let ((tail (nfa-new-state graph))
+        (head (nfa-new-state graph)))
+    (mapcar #'(lambda (c)
+                (nfa-new-edge graph tail head c))
+            args)
+    (nfa-new-nodes nil tail head)))
+
+(defun nfa-string (graph str)
+  (reduce #'(lambda (a b) (nfa--dot graph a b))
+          (map 'list #'(lambda (a) (nfa-simple-symbol graph a)) str)))
